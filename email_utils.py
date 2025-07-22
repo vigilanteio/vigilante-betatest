d42b23e863abc13b1065d7fccb16f8f6652a03cd
vigilanteio <marlonjavierblanco@gmail.com>
2025-07-22 21:47:41 +0100

    email_utils.py

--- email_utils.py
+++ email_utils.py
@@ -1,11 +1,19 @@
-import smtplib
-from email.mime.text import MIMEText
+import sendgrid
+from sendgrid.helpers.mail import Mail
 
-def enviar_email(destinatario, asunto, cuerpo, remitente, password):
-    msg = MIMEText(cuerpo)
-    msg["Subject"] = asunto
-    msg["From"] = remitente
-    msg["To"] = destinatario
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-        server.login(remitente, password)
-        server.sendmail(remitente, destinatario, msg.as_string())
+SENDGRID_API_KEY = "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"
+
+def enviar_email(destinatario, asunto, cuerpo, remitente):
+    sg = sendgrid.SendGridAPIClient(api_key=SENDGRID_API_KEY)
+    message = Mail(
+        from_email=remitente,
+        to_emails=destinatario,
+        subject=asunto,
+        plain_text_content=cuerpo
+    )
+    try:
+        response = sg.send(message)
+        return response.status_code
+    except Exception as e:
+        print(f"Error al enviar email: {e}")
+        return None
